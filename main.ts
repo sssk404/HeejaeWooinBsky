a8b32b4c84d5365724fd090f237cf3ac0643ab57
sssk404 <107937621+sssk404@users.noreply.github.com>
2024-11-15 14:01:04 +0900

    대사 추가 및 주기 변경(30분)

--- main.ts
+++ main.ts
@@ -21,8 +21,9 @@ const messages: string[] = [
     "콰직. 유리 조각들을 밟고 방 안으로 들어갔다. 밟은 유리 조각들이. 어째서인지 조금, 즐겁게 느껴졌다.",
     "\"정부와 한국마력연구소의 야심작이죠. 아직 시험 단계라 대한민국에는 단 두 개밖에 없는 물건이기도 하고요.\"",
     "\"다 제 잘못이었어요.\"",
-    "\"제가 잘못한 겁니다. 제 고집 때문이니까요. 제가 그렇게 억지를 쓰지만 않았어도....\""
-
+    "\"제가 잘못한 겁니다. 제 고집 때문이니까요. 제가 그렇게 억지를 쓰지만 않았어도....\"",
+    "원래 어린 시절 경험이 평생 가는 법이다.",
+    "세상 잘난 맛에 살던 소장이 빌빌거리는 꼴을 보아라!"
 ];
 
 // 무작위 메시지 출력
@@ -49,9 +50,9 @@ main();
 
 
 // Run this on a cron job
-const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+// const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '*/30 * * * *'; // Run once every 1 hr in prod
 
-const job = new CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
 
 job.start();
