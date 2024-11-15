f30785ff752ab8405556bd89afb475f6101f82c9
sssk404 <107937621+sssk404@users.noreply.github.com>
2024-11-15 13:49:18 +0900

    기본 대사 추가(시험용)

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,41 +31,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var api_1 = require("@atproto/api");
-var dotenv = require("dotenv");
-var cron_1 = require("cron");
-var process = require("process");
+const api_1 = require("@atproto/api");
+const dotenv = __importStar(require("dotenv"));
+const cron_1 = require("cron");
+const process = __importStar(require("process"));
 dotenv.config();
 // Create a Bluesky Agent 
-var agent = new api_1.AtpAgent({
+const agent = new api_1.AtpAgent({
     service: 'https://bsky.social',
 });
 // function getRandomInt(min: number, max: number): number {
@@ -50,45 +46,32 @@ var agent = new api_1.AtpAgent({
 //     max = Math.floor(max);
 //     return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
-var messages = [
-    "테스트를 해요",
-    "이게 맞나",
-    "저게 맞나",
-    "모르겠어요"
+const messages = [
+    "콰직. 유리 조각들을 밟고 방 안으로 들어갔다. 밟은 유리 조각들이. 어째서인지 조금, 즐겁게 느껴졌다.",
+    "\"정부와 한국마력연구소의 야심작이죠. 아직 시험 단계라 대한민국에는 단 두 개밖에 없는 물건이기도 하고요.\"",
+    "\"다 제 잘못이었어요.\"",
+    "\"제가 잘못한 겁니다. 제 고집 때문이니까요. 제가 그렇게 억지를 쓰지만 않았어도....\""
 ];
 // 무작위 메시지 출력
 function getRandomMessage() {
-    var randomIndex = Math.floor(Math.random() * messages.length);
+    const randomIndex = Math.floor(Math.random() * messages.length);
     return messages[randomIndex];
 }
 function main() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: 
-                // const
-                // const randomInt = getRandomInt(1, 10);
-                // console.log(randomInt);
-                return [4 /*yield*/, agent.login({ identifier: process.env.BLUESKY_USERNAME, password: process.env.BLUESKY_PASSWORD })];
-                case 1:
-                    // const
-                    // const randomInt = getRandomInt(1, 10);
-                    // console.log(randomInt);
-                    _a.sent();
-                    return [4 /*yield*/, agent.post({
-                            text: getRandomMessage()
-                        })];
-                case 2:
-                    _a.sent();
-                    console.log("리포봇이 움직였어요...");
-                    return [2 /*return*/];
-            }
+    return __awaiter(this, void 0, void 0, function* () {
+        // const
+        // const randomInt = getRandomInt(1, 10);
+        // console.log(randomInt);
+        yield agent.login({ identifier: process.env.BLUESKY_USERNAME, password: process.env.BLUESKY_PASSWORD });
+        yield agent.post({
+            text: getRandomMessage()
         });
+        console.log("리포봇이 움직였어요...");
     });
 }
 main();
 // Run this on a cron job
-var scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-var scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
-var job = new cron_1.CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+const job = new cron_1.CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
 job.start();
